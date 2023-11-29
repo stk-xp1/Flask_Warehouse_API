4f52bb490bbd351439f16d5a6ea9d105ed9a7a44
xbit <hadi@gmx.se>
2023-11-29 19:24:51 +0100

    Update SQL

--- warehouse.sql
+++ warehouse.sql
@@ -1,9 +1,8 @@
-
 CREATE DATABASE IF NOT EXISTS warehouse;
 USE warehouse;
 
-
 DROP TABLE IF EXISTS products, customers, staff, orders;
+
 CREATE TABLE products (
     id INT NOT NULL AUTO_INCREMENT,  
     name VARCHAR(255) NOT NULL,
@@ -12,7 +11,7 @@ CREATE TABLE products (
     created_at DATE NULL, 
     updated_at DATE NULL,
     PRIMARY KEY (id)
-)ENGINE = InnoDB;
+) ENGINE = InnoDB;
 
 CREATE TABLE customers (
     id INT NOT NULL AUTO_INCREMENT, 
@@ -28,7 +27,7 @@ CREATE TABLE customers (
 
 CREATE TABLE staffs (
     id INT NOT NULL AUTO_INCREMENT,
-    fist_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL, 
     employee_since DATE NULL, 
     age INT NOT NULL,
@@ -42,7 +41,6 @@ CREATE TABLE orders (
    product_id INT NOT NULL,
    customer_id INT NOT NULL, 
    staff_id INT NOT NULL, 
-   count INT NOT NULL, 
    created_at DATE NULL,
    updated_at DATE NULL, 
    PRIMARY KEY (id),
@@ -67,18 +65,14 @@ INSERT INTO products VALUES
     (1001,'Såg',120,13,'2022-01-10','2022-01-10'),
     (1002,'Snus',48,300,'2022-01-10','2022-01-10');
 
-
 INSERT INTO customers VALUES 
     (101,'Anders','Garcia','Ulla lyckas väg 13',43212,47,'2022-01-10','2022-01-10'),
     (102,'Göran','Persson','Storås vägen 73',41254,29,'2022-01-10','2022-01-10');
-
-
 
 INSERT INTO staffs VALUES 
     (1,'Unni','Lozic','2001-10-12',47,'2022-01-10','2022-01-10'),
     (2,'Liv','Strömberg','2012-03-27',32,'2022-01-10','2022-01-10');
 
-
 INSERT INTO orders VALUES 
-    (1,1002,103,1002,1, '2022-01-10','2022-01-10'),
-    (2,1004,103,1002,10, '2022-01-10','2022-01-10');
+    (1,1002,103,1002,'2022-01-10','2022-01-10'),
+    (2,1004,103,1002,'2022-01-10','2022-01-10');
